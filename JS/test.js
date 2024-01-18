e5ea5d1d5cb76dd440f6bebea56ccba83c176120
dkaprdtya <dkaprdtya@gmail.com>
2024-01-18 23:43:55 +0800

    ya

--- JS/test.js
+++ JS/test.js
@@ -2,20 +2,24 @@ function square( )
 {
 var side,area;
 side = parseInt(document.getElementById("first").value);
-area = side*side; //Equation for Area of a square
+area = side*side;
+document.getElementById("num").innerHTML= side + " x " + side
 document.getElementById("num").innerHTML="Luas Persegi adalah : "+area;
 }
+
 function keliling( )
 {
 var sisi,perimeter;
 sisi = parseInt(document.getElementById("second").value);
-perimeter = 4*sisi; //Equation for Perimeter of a squaredocument.getElementById("num1").innerHTML="Keliling Persegi adalah : "+perimeter;
+perimeter = 4*sisi;
 document.getElementById("num1").innerHTML="Keliling Persegi adalah : "+perimeter;
 }
+
 function fun1() {
     const first = document.querySelector('#first')
     first.value = null;
   }
+
   function fun2() {
     const first = document.querySelector('#second')
     first.value = null;
