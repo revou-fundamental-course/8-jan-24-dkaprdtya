7b7bd91fea7c04ff68de99421b9e268f467ca4d7
dkaprdtya <dkaprdtya@gmail.com>
2024-01-18 21:57:08 +0800

    new

--- JS/test.js
+++ JS/test.js
@@ -1,11 +1,23 @@
-const inputField = document.getElementById("squareSide")
-const countButton = document.getElementById("countButton")
+function square( )
+{
+var side,area;
+side = parseInt(document.getElementById("first").value);
+area = side*side; //Equation for Area of a square
+document.getElementById("num").innerHTML="Luas Persegi adalah : "+area;
+}
 
-inputField.addEventListener('input', function() {
-    const inputValue = inputField.ariaValueMax.trim()
-    if(validateInput(inputValue)) {
-        countButton.disabled = false
-    } else {
-         countButton.disabled = true
-         }
-})
+function keliling( )
+{
+var sisi,perimeter;
+sisi = parseInt(document.getElementById("second").value);
+perimeter = 4*sisi; //Equation for Perimeter of a squaredocument.getElementById("num1").innerHTML="Keliling Persegi adalah : "+perimeter;
+document.getElementById("num1").innerHTML="Keliling Persegi adalah : "+perimeter;
+}
+function fun1() {
+    const first = document.querySelector('#first')
+    first.value = null;
+  }
+  function fun2() {
+    const first = document.querySelector('#second')
+    first.value = null;
+  }
